e7568b9fc78f12b02f48db810c1e8c943103be10
Jayanta Biswas <bjayanta.me@gmail.com>
2020-11-29 01:17:42 +0600

    update index page

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,18 @@
       <nuxt-link class="btn btn-primary btn-lg" to="/posts" role="button">Posts</nuxt-link>
     </div>
 
-    <div class="card-deck d-flex justify-content-center flex-wrap">
-      <PostPreview id="1" title="Hello World!" thumbnail="https://via.placeholder.com/350x250" text="Say you bought a new phone, but at the end of each day, without fail, the operating system crashed. Would you keep using it? Of course not. You’d take the phone back to the store, complain, and get a new one. And yet many people run their entire lives on a faulty operating system. It’s called the to-do list." />
-      <PostPreview id="2" title="Hello World!" thumbnail="https://via.placeholder.com/350x250" text="Say you bought a new phone, but at the end of each day, without fail, the operating system crashed. Would you keep using it? Of course not. You’d take the phone back to the store, complain, and get a new one. And yet many people run their entire lives on a faulty operating system. It’s called the to-do list." />
-      <PostPreview id="3" title="Hello World!" thumbnail="https://via.placeholder.com/350x250" text="Say you bought a new phone, but at the end of each day, without fail, the operating system crashed. Would you keep using it? Of course not. You’d take the phone back to the store, complain, and get a new one. And yet many people run their entire lives on a faulty operating system. It’s called the to-do list." />
-    </div>
+    <section class="features">
+      <div class="features-title">
+        <h3>Features Posts</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ipsum praesentium, unde doloremque quae odio in dolorum molestias repudiandae facere dolore quisquam quos nam aspernatur fugiat harum voluptate necessitatibus asperiores.</p>
+      </div>
+
+      <div class="card-deck d-flex justify-content-center flex-wrap">
+        <PostPreview id="1" title="Hello World!" thumbnail="https://via.placeholder.com/350x250" text="Say you bought a new phone, but at the end of each day, without fail, the operating system crashed. Would you keep using it? Of course not. You’d take the phone back to the store, complain, and get a new one. And yet many people run their entire lives on a faulty operating system. It’s called the to-do list." />
+        <PostPreview id="2" title="Hello World!" thumbnail="https://via.placeholder.com/350x250" text="Say you bought a new phone, but at the end of each day, without fail, the operating system crashed. Would you keep using it? Of course not. You’d take the phone back to the store, complain, and get a new one. And yet many people run their entire lives on a faulty operating system. It’s called the to-do list." />
+        <PostPreview id="3" title="Hello World!" thumbnail="https://via.placeholder.com/350x250" text="Say you bought a new phone, but at the end of each day, without fail, the operating system crashed. Would you keep using it? Of course not. You’d take the phone back to the store, complain, and get a new one. And yet many people run their entire lives on a faulty operating system. It’s called the to-do list." />
+      </div>
+    </section>
   </section>
 </template>
 
@@ -27,4 +34,7 @@ export default {
 </script>
 
 <style scoped>
+.features .features-title {
+  text-align: center;
+}
 </style>
