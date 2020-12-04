d53d495d0e8271e7ffeeb75cdca9f63fb1d123fc
Jayanta <bjayanta.me@gmail.com>
2020-12-04 15:23:51 +0600

    update frontend design.

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,35 +1,36 @@
 <template>
     <section class="container">
-        <h1>About</h1>
-        <h3>Learn more about the team maintaining Bootstrap, how and why the project started, and how to get involved.</h3>
+        <div class="about">
+            <h1>About</h1>
+            <h3>Learn more about the team maintaining Bootstrap, how and why the project started, and how to get involved.</h3>
 
-        <article>
-            <h2>Team</h2>
-            <p>Bootstrap is maintained by a small team of developers on GitHub. We’re actively looking to grow this team and would love to hear from you if you’re excited about CSS at scale, writing and maintaining vanilla JavaScript plugins, and improving build tooling processes for frontend code.</p>
-        </article>
+            <article>
+                <h2>Team</h2>
+                <p>Bootstrap is maintained by a small team of developers on GitHub. We’re actively looking to grow this team and would love to hear from you if you’re excited about CSS at scale, writing and maintaining vanilla JavaScript plugins, and improving build tooling processes for frontend code.</p>
+            </article>
 
-        <article>
-            <h2>History</h2>
-            <p>Originally created by a designer and a developer at Twitter, Bootstrap has become one of the most popular front-end frameworks and open source projects in the world.</p>
+            <article>
+                <h2>History</h2>
+                <p>Originally created by a designer and a developer at Twitter, Bootstrap has become one of the most popular front-end frameworks and open source projects in the world.</p>
 
-            <p>Bootstrap was created at Twitter in mid-2010 by @mdo and @fat. Prior to being an open-sourced framework, Bootstrap was known as Twitter Blueprint. A few months into development, Twitter held its first Hack Week and the project exploded as developers of all skill levels jumped in without any external guidance. It served as the style guide for internal tools development at the company for over a year before its public release, and continues to do so today.</p>
+                <p>Bootstrap was created at Twitter in mid-2010 by @mdo and @fat. Prior to being an open-sourced framework, Bootstrap was known as Twitter Blueprint. A few months into development, Twitter held its first Hack Week and the project exploded as developers of all skill levels jumped in without any external guidance. It served as the style guide for internal tools development at the company for over a year before its public release, and continues to do so today.</p>
 
-            <p>Originally released on Friday, August 19, 2011, we’ve since had over twenty releases, including two major rewrites with v2 and v3. With Bootstrap 2, we added responsive functionality to the entire framework as an optional stylesheet. Building on that with Bootstrap 3, we rewrote the library once more to make it responsive by default with a mobile first approach.</p>
+                <p>Originally released on Friday, August 19, 2011, we’ve since had over twenty releases, including two major rewrites with v2 and v3. With Bootstrap 2, we added responsive functionality to the entire framework as an optional stylesheet. Building on that with Bootstrap 3, we rewrote the library once more to make it responsive by default with a mobile first approach.</p>
 
-            <p>With Bootstrap 4, we once again rewrote the project to account for two key architectural changes: a migration to Sass and the move to CSS’s flexbox. Our intention is to help in a small way to move the web development community forward by pushing for newer CSS properties, fewer dependencies, and new technologies across more modern browsers.</p>
-        </article>
+                <p>With Bootstrap 4, we once again rewrote the project to account for two key architectural changes: a migration to Sass and the move to CSS’s flexbox. Our intention is to help in a small way to move the web development community forward by pushing for newer CSS properties, fewer dependencies, and new technologies across more modern browsers.</p>
+            </article>
 
-        <article>
-            <h2>Get involved</h2>
-            <p>Get involved with Bootstrap development by opening an issue or submitting a pull request. Read our contributing guidelines for information on how we develop.</p>
-        </article>
+            <article>
+                <h2>Get involved</h2>
+                <p>Get involved with Bootstrap development by opening an issue or submitting a pull request. Read our contributing guidelines for information on how we develop.</p>
+            </article>
+        </div>
     </section>
 </template>
 
 <style scoped>
-.about-page {
-    text-align: center;
-    padding: 30px; 
+.about {
+    margin-top: 70px;
 }
 
 </style>
